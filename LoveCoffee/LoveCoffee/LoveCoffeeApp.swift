259527d70a0d827f1add141d521798d9a746d1f3
Isaac Ramirez <iramirezc@live.com.mx>
2022-03-19 15:37:50 -0600

    Add LoveCoffee swift example app

---
+++ LoveCoffee/LoveCoffee/LoveCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveCoffeeApp.swift
+//  LoveCoffee
+//
+//  Created by Isaac Ramirez on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LoveCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
